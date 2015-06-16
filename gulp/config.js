433f0245dda3dd64dd7b31d09f5a68ecadf9f150
Christophe Massolin <christophe.massolin@gmail.com>
2015-06-17 00:55:47 +0200

    First Commit

---
+++ gulp/config.js
@@ -0,0 +1,85 @@
+var srcPath = 'app/'
+var dstPath = 'public/'
+
+module.exports = {
+  global: {
+    javascript_compiler: 'babel' // es6 or coffee
+  }
+
+  ,html: {
+    src:   dstPath+'*.html'
+    ,opts: {
+      cwd: dstPath
+    }
+  }
+
+  ,watch: {
+    files: [dstPath+'**/*.html', dstPath+'shaders/**']
+  }
+
+  ,coffee: {
+    src:     srcPath+'scripts/**/*.coffee'
+    ,source: srcPath+'scripts/main.coffee'
+    ,dst:    dstPath+'scripts/'
+    ,opts:   {
+      bare: true
+    }
+  }
+
+  ,babel: {
+    src:      srcPath+'scripts/**/*.{es,es6,js,babel}'
+    ,dst:     dstPath+'scripts/'
+    ,options: {
+      dev: {
+        modules: "common"
+      }
+      ,dist: {
+        modules: "common"
+        ,compact: true
+        ,comments: false
+      }
+    }
+  }
+
+  ,sass: {
+    src:      srcPath+'styles/**/*.{scss,sass}'
+    ,srcPath: srcPath+'styles/'
+    ,dstPath: dstPath+'styles/'
+    ,options: {
+      dev: {
+        sourcemap: true
+      }
+      ,dist: {
+        sourcemap: false
+        ,style: "compressed"
+      }
+    }
+  }
+
+  ,browserSync: {
+    open:          false
+    ,watchOptions: {
+      debounceDelay: 1000
+    }
+    ,server: { baseDir: './public' }
+    // ,proxy:  'spacejukebox.dev'
+  }
+
+  ,browserify: {
+    opts: {
+      debug: true
+      ,entries: [srcPath+'scripts/main.es']
+    }
+    ,modulePath:  srcPath+'scripts/_modules/'
+    ,extensions: ['.js']
+    ,source:     'main.js'
+    ,dstPath:    dstPath+'scripts/'
+  }
+
+  ,vendor: {
+    scripts: {
+      source:   'vendors.js' 
+      ,dstPath: dstPath+'scripts/'
+    }
+  }
+}
---
+++ gulp/tasks/browserify.js
@@ -0,0 +1,63 @@
+var gulp       = require('gulp');
+var config     = require('../config').browserify;
+var babelCfg   = require('../config').babel;
+var source     = require('vinyl-source-stream');
+var buffer     = require('vinyl-buffer');
+var babelify   = require('babelify');
+var watchify   = require('watchify');
+var browserify = require('browserify');
+var notify     = require('gulp-notify');
+var reload     = require('browser-sync').reload;
+var _          = require('lodash');
+var gif        = require('gulp-if');
+var uglify     = require('gulp-uglify');
+
+gulp.task('browserify', function(){
+    bundle({ watch: false, minify: false });
+});
+
+gulp.task('browserify:build', function(){
+    bundle({ watch: false, minify: true });
+});
+
+gulp.task('browserify:watch', function(){
+    bundle({ watch: true, minify: false });
+});
+
+function bundle(options){
+
+    var watch = options.watch,
+    minify    = options.minify,
+    b         = null;
+
+    if (watch) {
+        _.assign(watchify.args, config.opts);
+        b = watchify(browserify(watchify.args));
+        b.on('update', rebundle);
+    } else {
+        b = browserify(config.opts);
+    }
+
+    if (minify) {
+        b.transform(babelify.configure(babelCfg.options.dist));
+    } else {
+        b.transform(babelify.configure(babelCfg.options.dev));
+    }
+
+    function rebundle(){
+        return b.bundle()
+                .on('error', notify.onError(handleError))
+                .pipe(source(config.source))
+                .pipe(buffer())
+                .pipe(gif(minify, uglify()))
+                .pipe(gulp.dest(config.dstPath))
+                .pipe(gif(watch, reload({ stream: true })));
+    }
+
+    function handleError(error){
+        return 'ERROR: ' + error;
+    }
+
+    rebundle();
+
+}
